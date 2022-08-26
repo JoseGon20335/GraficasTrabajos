e90500cd8a37366e56c27303df218a9ee07c2fcb
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-08-25 19:12:09 -0600

    sr3working

---
+++ sr3/main.py
@@ -0,0 +1,18 @@
+from gl import Renderer
+
+
+def main():
+
+    glO = Renderer()
+    glO.glInit()
+    glO.glColor(1, 1, 1)
+    glO.glClearColor(0, 0, 0)
+    glO.glCreateWindow(1000, 1000)
+    glO.glViewPort(0, 0, 1000, 1000)
+
+    glO.glLoad('./models/hollowknight.obj', (0, -1.3), (0.45, 0.45))
+
+    glO.glFinish()
+
+
+main()
