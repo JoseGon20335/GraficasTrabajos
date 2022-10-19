943c523260ebf7931420a9ea5dbf2d4531843b70
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-10-19 16:07:39 -0600

    sr5funciona

--- SR/sr5-texturas/main.py
+++ SR/sr5-texturas/main.py
@@ -11,9 +11,18 @@ def main():
     glO.glCreateWindow(800, 800)
     glO.glViewPort(0, 0, 500, 500)
 
-    texture = Texture('./models/figura.bmp')
-    glO.glLoad('./models/figura.obj', (0, -4, 0),
-               (130, 130, 130), texture=texture)
+    # texture = Texture('./models/figura.bmp')
+    # glO.glLoad('./models/figura.obj', (0, -4, 0),
+    #            (130, 130, 130), texture=texture)
+
+    # texture = Texture('./models/TIE.bmp')
+    # glO.glLoad('./models/TIE.obj', (0, -4, 0),
+    #            (130, 130, 130), texture=texture)
+
+    texture = Texture('./models/xwing.bmp')
+    glO.glLoad('./models/xwing.obj', (6, 6, 6),
+               (50, 50, 50), texture=texture)
+
     glO.glFinish()
 
 
