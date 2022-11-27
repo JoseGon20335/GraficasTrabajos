a87fdb0003ef29d93c9b936951da8188ea0c71d8
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-11-27 17:14:12 -0600

    angels

--- SR/sr6-camaras/main.py
+++ SR/sr6-camaras/main.py
@@ -8,30 +8,36 @@ V4 = namedtuple('Point4D', ['x', 'y', 'z', 'w'])
 
 
 def main():
-
     glO = Renderer()
     glO.glInit()
     glO.glColor(1, 1, 1)
     glO.glClearColor(0, 0, 0)
-    glO.glCreateWindow(800, 800)
-    glO.glViewPort(0, 0, 800, 800)
+    glO.glCreateWindow(1000, 1000)
+    glO.glViewPort(0, 0, 1000, 1000)
 
-    # medium angel
-    # glO.look(V3(0, -0, 40), V3(0, -2, 3), V3(0, 9, 9))
-    # texture = Texture('./models/mo.bmp')
-    # glO.glLoad('./models/mo.obj', (-1, -3, 0),
+    # Medium shot
+    # glO.look(V3(0, 0, 20), V3(0, 1, 0), V3(0, 1, 1))
+    # texture = Texture('./models/link.bmp')
+    # glO.glLoad('./models/link.obj', (-1, -2, 0),
     #            (0.5, 0.5, 0.5), (0, 0, 0), texture=texture)
 
-    # low angel
-    # glO.look(V3(0, 0, 40), V3(0, -3, 3), V3(0, 9, 9))
-    # texture = Texture('./models/mo.bmp')
-    # glO.glLoad('./models/mo.obj', (-1, -3, 0),
-    #            (0.5, 0.5, 0.5), (0, 0, 0), texture=texture)
+    # Low angle
+    # glO.look(V3(2, -5, 10), V3(0, 0, 0), V3(0, 1, 1))
+    # texture = Texture('./models/link.bmp')
+    # glO.glLoad('./models/link.obj', (-1, -2.5, 0),
+    #            (0.8, 0.8, 0.8), (0, 0, 0), texture=texture)
 
-    glO.look(V3(0, 0, 40), V3(0, -1, 3), V3(0, 9, 9))
-    texture = Texture('./models/mo.bmp')
-    glO.glLoad('./models/mo.obj', (-1, -3, 0),
-               (0.5, 0.5, 0.5), (0, 0, 0), texture=texture)
+    # High angle
+    # glO.look(V3(0, 9.5, 20), V3(0, 1, 0), V3(0, 1, 1))
+    # texture = Texture('./models/link.bmp')
+    # glO.glLoad('./models/link.obj', (-1, -2.8, 0),
+    #            (0.7, 0.7, 0.7), (0, 0, 0), texture=texture)
+
+    # Dutch angle
+    glO.look(V3(9, -5, 10), V3(0, 0, 0), V3(0, 1, 1))
+    texture = Texture('./models/link.bmp')
+    glO.glLoad('./models/link.obj', (-1, -3, 0),
+               (0.8, 0.8, 0.8), (0, -1, 0), texture=texture)
 
     glO.glFinish()
 
